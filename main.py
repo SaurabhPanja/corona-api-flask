0c12ddaa4856b3d74175f7b6809003bb3df92e2d
Saurabh Panja <saurabh@market-pulse.in>
2020-03-30 16:27:58 +0530

    can get province data from url

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ app = Flask(__name__)
 def get_corona_api():
     return corona_data_api
 
+@app.route('/<string:province>')
+def get_data(province):
+    try:
+        province_data = corona_data_api[province]
+    except:
+        province_data = {"message": "Invalid request. No such province found."}
+    
+    return province_data
+
 if __name__ == '__main__':
    app.run()
